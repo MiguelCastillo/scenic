b1b673b9063d4a79e5f76be2d64c6f1c5c7bb0be
MiguelCastillo <manchagnu@gmail.com>
2021-12-24 08:46:50 -0500

    Flipped texture rendering along the Y axis
    Rendering textures that are loaded with new Image() are loaded up side down. So we flip that so that those images look correct.
    As long as image buffer are stored in the same order as images loaded with new Image(), we can just keep this as default behavior. Otherwise, we are going to need to add a way to flip images more selectively on a per texture basis which can add lots of unwanted complexity because we will need to keep correctly push and pop that configuration from the state stack so that textures don't interfere with each other.
    
    Helpful context: https://jameshfisher.com/2020/10/22/why-is-my-webgl-texture-upside-down/

--- editor/render-loop.js
+++ editor/render-loop.js
@@ -10,6 +10,18 @@ export function startRenderLoop(gl, updateScene, renderScene) {
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
 
+    // Rendering textures that are loaded with new Image() are loaded
+    // up side down. So we flip that so that those images look correct.
+    // As long as image buffer are stored in the same order as images
+    // loaded with new Image(), we can just keep this as default behavior.
+    // Otherwise, we are going to need to add a way to flip images more
+    // selectively on a per texture basis which can add lots of unwanted
+    // complexity because we will need to keep correctly push and pop 
+    // that configuration from the state stack so that textures don't
+    // interfere with each other.
+    // https://jameshfisher.com/2020/10/22/why-is-my-webgl-texture-upside-down/
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
     // WebGL default frontface is gl.CCW.
     // https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/frontFace
     // gl.frontFace(gl.CW);
