6ebc7d0fcfbcf64f056adf0e6cdaeb07c281e92b
MiguelCastillo <manchagnu@gmail.com>
2022-01-08 10:22:35 -0500

    Revamped keyframe animation functionality
    As Im working thru adding animation support for FBX files, I need to enhance the support for the keyframe animation utilities I already have.  So I have added three mathods for animation 2v, 3v, and scalars. I have also revamped how animation keyframe is calculate so that it is stateless and animation is always derrived at time progresses.  The current implementation does have the draw back that animation ought to start at 0 milliseconds for the animation to start from the beginning. This needs to be more flexible so that we can start an animation at any given time.

--- src/animation/keyframe.js
+++ src/animation/keyframe.js
@@ -1,80 +1,145 @@
-export const keyframe = (frames, ease=(x) => x) => {
-  if (!frames || frames.length < 2) {
-    throw new Error("Frames must be an array with at least 2 items.");
+// Keyframe animation is based on segments that have a start and an end.
+// The start of a segment is the value of a frame and the end of that
+// same segment is the start of a the next frame. Where a segment ends is
+// exaclty where another one starts; they produce the same value. More on
+// this later.
+//
+// In an animation, we iterate thru these segments and want to do that
+// as smoothly as possible over time. In order to do that we interpolate
+// the values of the start and the end of a segement as time goes by. The
+// interpolation between these values are linear over time so we call it
+// linear interpolation. When we interpolate the start and end of a segment
+// we need to measure how much time has gone by since the start of the
+// segment. This measure is called delta and it is a value between 0 and 1
+// where 0 lies exactly on the start of the segment and 1 lies exactly at
+// end of the segment.
+//
+// To illustrate delta, let's assume for a minute that a segment is 1 second
+// long, meaning it takes 1 second to go from the start to the end. At 0
+// seconds we are at the very start of the segment. At 250 milliseconds we
+// have a 0.25 delta which is 1/4 of the way into the segment, at 500
+// milliseconds we have a delta of 0.5 and are at the midpoint of the segment
+// and so on. We can think of this also in terms of percentages where 250
+// milliseconds is 25% of the segment, 500 milliseconds is 50% of the segment
+// and so on. So 20% of a second is 0.2 milliseconds.
+// 
+// More concretely as illustrated below, `s` denotes a segment and we have 3
+// of them. So segment 1 starts at 0 seconds and ends at 1 second. Segment
+// 2 starts at 1 second and ends at 2 seconds. And so on. Now imagine that
+// frame 1 lies on the start of s1 and frame 2 lies on the start of segment
+// 2. When we interpolate with delta we are basically figuring out how much
+// of frame 1 and how much of frame 2 we have to use in order to  generate
+// the final frame. Without interpolation s1 is basically frame 1.
+//
+// * cp below are curve points, which is a common term used to describe each
+// data point in an animation curve.
+//
+//       s1         s2         s3
+//  |----------|----------|----------|
+// cp0        cp1        cp2        cp3
+//
+
+
+// https://www.khanacademy.org/computing/pixar/animate/ball/pi/animation-with-linear-interpolation
+// https://www.freecodecamp.org/news/understanding-linear-interpolation-in-ui-animations-74701eb9957c/
+const lerp = (fraction, min, max, ease=(x) => x) => {
+  //const length = Math.sqrt(min*min + max*max);
+  return ease(((max - min) * fraction) + min);
+}
+
+export const animateScalar = (frames, ease) => {
+  const controller = animationCurveController(frames.length);
+
+  return (ms, speed) => {
+    const [delta, index] = controller(ms, speed);
+    return lerp(delta, frames[index], frames[index+1], ease);
+  };
+};
+
+export const animate2v = (frames, ease) => {
+  const controller = animationCurveController(frames.length);
+
+  return (ms, speed) => {
+    const [delta, index] = controller(ms, speed);
+    return [
+      lerp(delta, frames[index][0], frames[index+1][0], ease),
+      lerp(delta, frames[index][1], frames[index+1][1], ease),
+    ];
+  };
+};
+
+export const animate3v = (frames, ease) => {
+  const controller = animationCurveController(frames.length);
+
+  return (ms, speed) => {
+    const [delta, index] = controller(ms, speed);
+    return [
+      lerp(delta, frames[index][0], frames[index+1][0], ease),
+      lerp(delta, frames[index][1], frames[index+1][1], ease),
+      lerp(delta, frames[index][2], frames[index+1][2], ease),
+    ];
+  };
+};
+
+export const keyframe = animate3v;
+
+function animationCurveController(curvePointCount) {
+  if (curvePointCount < 2) {
+    throw new Error("there must be at least 2 markers.");
   }
 
-  // https://www.khanacademy.org/computing/pixar/animate/ball/pi/animation-with-linear-interpolation
-  // https://www.freecodecamp.org/news/understanding-linear-interpolation-in-ui-animations-74701eb9957c/
-  const lerp = (min, max, fraction) => {
-    //const length = Math.sqrt(min*min + max*max);
-    return ease(((max - min) * fraction) + min);
-  }
-
-  let prevMs;
-  let currentFrame;
-  let nextFrame;
+  const segmentCount = curvePointCount - 1;
 
   // The way this works is that ms is time in millisenconds
   // that is always advancing. And every second we will increase
   // or decrease the current frame depending on whether we are
   // going forward or backward with animation.
-  // One way to think about frames here is like a window that's
-  // based on the current frame and the next frame. That window
-  // will move back or forward depending on the direction of the
-  // animation. And within that window, we will lerp left or
-  // or right depending on the direction of the animation.
   return (ms, speed=1) => {
-    ms = Math.floor(ms * Math.abs(speed));
+    // Slow or speed things up! We also take the floor because
+    // the decimal points can cause jitters in animations.
+    ms = Math.abs(Math.floor(ms * speed));
 
-    // First time this thing runs. Let's initialize local state!
-    if (currentFrame === undefined) {
-      prevMs = ms;
-      currentFrame = 0;
-      nextFrame = currentFrame + 1;
+    // Each second is where markers are for each frame. So dividing
+    // milliseconds by 1000 tells us the frame. We normalize the frame
+    // number to array indexes tho because these eventually map to arrays
+    // with frame data. curvePointIndex always points to the beginning
+    // of the segment.
+    let curvePointIndex = Math.floor(ms * 0.001);
+
+    // delta tells us where within a frame range we are so that we can
+    // interpolate data within frames each second.
+    // We use division here instead of 0.001 multiplication because
+    // that handles floating point seemingly better. A particular example
+    // that causes tests to fail when ms is 9.
+    // 9 * 0.001 yields 0.009000000000000001. But it really should be 0.009.
+    // https://techformist.com/problems-with-decimal-multiplication-javascript/
+    //
+    // TODO(miguel): handle time spans greater than one second.
+    let delta = (ms % 1000)*0.001;
+
+    //
+    //  |----------|----------|
+    // cp0        cp1        cp2
+    //
+    if (!delta && curvePointIndex) {
+      // This adjustment allows us to access all the data in the very
+      // last frame. Basically instead of taking frame X with delta
+      // 0, we take frame - X with delta of 1. It will yield the same
+      // result, but we do this adjusment so that we can access the very
+      // last datapoint in a frame while staying withing range in the
+      // array of frame data. 
+      delta = 1;
+      curvePointIndex--;
     }
 
-    let timeDelta = ms - prevMs;
+    curvePointIndex = curvePointIndex % segmentCount;
 
-    // Every second we will increment the frame. But within each second
-    // we will lerp so that we can get a smooth animation.
-    // When we advance each frame, it is not always the case that exactly
-    // 1 second has elapsed since the last frame. So we take the current
-    // time delta, and will subtract 1 sec. The remainder is used to
-    // lerp part of the new frame to provide a smooth transition between
-    // frames even when the system does not advance each frame exactly
-    // every second.
-    if (timeDelta > 1000) {
-      if (speed >= 0) {
-        currentFrame++;
-        if (currentFrame === frames.length - 1) {
-          currentFrame = 0;
-        }
-      } else {
-        currentFrame--;
-        if (currentFrame === -1) {
-          currentFrame = frames.length - 2;
-        }
-      }
-
-      timeDelta -= 1000;
-      nextFrame = currentFrame + 1;
-      prevMs = ms;
+    if (speed < 0) {
+      // If we are in reverse mode then we just invert the current state.
+      curvePointIndex = segmentCount - curvePointIndex - 1;
+      delta = 1-delta;
     }
 
-
-    // If the speed is positive that means that delta is going
-    // to increase over time from 0 to 1. If speed is negative
-    // then delta will decrease over time from 1 to 0. That
-    // allows us to come up with multipliers for lerp that move
-    // frame interpolation forward or backward.
-    let delta = speed >= 0 ? timeDelta/1000 : (1 - timeDelta/1000);
-
-    const result = [
-      lerp(frames[currentFrame][0], frames[nextFrame][0], delta),
-      lerp(frames[currentFrame][1], frames[nextFrame][1], delta),
-      lerp(frames[currentFrame][2], frames[nextFrame][2], delta),
-    ];
-
-    return result;
-  };
+    return [delta, curvePointIndex];
+  }
 }
--- src/animation/keyframe.test.js
+++ src/animation/keyframe.test.js
@@ -1,19 +1,22 @@
-import {keyframe} from "./keyframe.js";
+import {
+  animate3v,
+  animateScalar,
+} from "./keyframe.js";
 
-test("no frames throws error", () => {
+test("animate3v no frames throws error", () => {
   expect(() => {
-    keyframe([])
-  }).toThrowError(new Error("Frames must be an array with at least 2 items."));
+    animate3v([])
+  }).toThrowError(new Error("there must be at least 2 markers."));
 });
 
-test("with 1 frame throws error", () => {
+test("animate3v with 1 frame throws error", () => {
   expect(() => {
-    keyframe([[6,7,8]])
-  }).toThrowError(new Error("Frames must be an array with at least 2 items."));
+    animate3v([[6,7,8]])
+  }).toThrowError(new Error("there must be at least 2 markers."));
 });
 
-test("with 2 frames with multiple MS interval from 0 to 1 sec", () => {
-  const animator = keyframe([[1, 2, 3], [6, 7, 8]]);
+test("animate3v with 2 frames with multiple MS interval from 0 to 1 sec", () => {
+  const animator = animate3v([[1, 2, 3], [6, 7, 8]]);
 
   // (6-1)*0 + 1 = 0 + 1 = 1
   // (7-2)*0 + 2 = 0 + 2 = 2
@@ -80,8 +83,8 @@ test("with 2 frames with multiple MS interval from 0 to 1 sec", () => {
   expect(animator(1001)).toEqual([1.005, 2.005, 3.005]);
 });
 
-test("with 2 frames with multiple MS interval from 0 to 1 sec in reverse", () => {
-  const animator = keyframe([[1, 2, 3], [6, 7, 8]]);
+test("animate3v with 2 frames with multiple MS interval from 0 to 1 sec in reverse", () => {
+  const animator = animate3v([[1, 2, 3], [6, 7, 8]]);
 
   // (6-1)*1 + 1 = 5 + 1 = 6
   // (7-2)*1 + 2 = 6 + 2 = 7
@@ -113,3 +116,129 @@ test("with 2 frames with multiple MS interval from 0 to 1 sec in reverse", () =>
   // (8-3)*(.999) + 3 = 5*(0.999) + 3 = 7.995
   expect(animator(1001, -1)).toEqual([5.995, 6.995, 7.995]);
 });
+
+test("animateScalar with 4 curve points and 10 second span with 1 millisecond at a time", () => {
+  const animator = animateScalar([0, 1, 2, 3]);
+
+  for (let i = -10000; i < 10000; i++) {
+    // Tricky end of frame values. This is just reference to help illustrate
+    // what values to expect and their relationship to each other.
+    // 3000/1000  = 3    // 3/3   = 1  // 3 frame value
+    // 6000/1000  = 6    // 6/3   = 2  // 3 frame value
+    // 9000/1000  = 9    // 9/3   = 3  // 3 frame value
+    // 12000/1000 = 12   // 12/3  = 4  // 3 frame value
+
+    let actual = animator(i);
+    let expected = Math.abs((i * 0.001)%3);
+    // This adjustment is to ensure that we have the right value when
+    // we are at the end of frame.
+    expected = !expected && i ? 3 : expected;
+    expected = normalizeDecimal(expected);
+
+    // Because of floating point math "stuff" in javascript, multiplication
+    // can yield bad results. For example
+    // 9 * 0.001 yields 0.009000000000000001. But it really should be 0.009.
+    // https://techformist.com/problems-with-decimal-multiplication-javascript/
+    //
+    // So we normalize the values to have the same decimal count to ensure
+    // values do match.
+    expect(normalizeDecimal(actual)).toEqual(normalizeDecimal(expected));
+  }
+});
+
+test("animateScalar with 4 curve points and 10 second span with 1 millisecond at a time - in reverse", () => {
+  const animator = animateScalar([0, 1, 2, 3]);
+  const speed = -1;
+
+  for (let i = -10000; i < 10000; i++) {
+    let actual = animator(i, speed);
+    let expected = Math.abs((i * 0.001)%3);
+    // This adjustment is to ensure that we have the right value when
+    // we are at the end of frame.
+    expected = !expected && i ? 3 : expected;
+    expected = normalizeDecimal(3 - expected);
+
+    // Because of floating point math "stuff" in javascript, multiplication
+    // can yield bad results. For example
+    // 9 * 0.001 yields 0.009000000000000001. But it really should be 0.009.
+    // https://techformist.com/problems-with-decimal-multiplication-javascript/
+    //
+    // So we normalize the values to have the same decimal count to ensure
+    // values do match.
+    expect(normalizeDecimal(actual)).toEqual(normalizeDecimal(expected));
+  }
+});
+
+test("animateScalar with 4 curve points iterating over a 3 second span", () => {
+  const animator = animateScalar([0, 1, 2, 3]);
+  const speed = 1;
+
+  expect(animator(0, speed)).toEqual(0);
+  expect(animator(250, speed)).toEqual(0.25);
+  expect(animator(500, speed)).toEqual(0.5);
+  expect(animator(750, speed)).toEqual(0.75);
+  expect(animator(1000, speed)).toEqual(1);
+  expect(animator(1250, speed)).toEqual(1.25);
+  expect(animator(1500, speed)).toEqual(1.5);
+  expect(animator(1750, speed)).toEqual(1.75);
+  expect(animator(2000, speed)).toEqual(2);
+  expect(animator(2250, speed)).toEqual(2.25);
+  expect(animator(2500, speed)).toEqual(2.5);
+  expect(animator(2750, speed)).toEqual(2.75);
+  expect(animator(3000, speed)).toEqual(3);
+  expect(animator(3001, speed)).toEqual(0.001);
+  expect(animator(3002, speed)).toEqual(0.002);
+});
+
+test("animateScalar with 4 curve points iterating over a 3 second span in reverse", () => {
+  const animator = animateScalar([0, 1, 2, 3]);
+  const speed = -1;
+
+  expect(animator(0, speed)).toEqual(3);
+  expect(animator(250, speed)).toEqual(2.75);
+  expect(animator(500, speed)).toEqual(2.5);
+  expect(animator(750, speed)).toEqual(2.25);
+  expect(animator(1000, speed)).toEqual(2);
+  expect(animator(1250, speed)).toEqual(1.75);
+  expect(animator(1500, speed)).toEqual(1.5);
+  expect(animator(1750, speed)).toEqual(1.25);
+  expect(animator(2000, speed)).toEqual(1);
+  expect(animator(2250, speed)).toEqual(0.75);
+  expect(animator(2500, speed)).toEqual(0.5);
+  expect(animator(2750, speed)).toEqual(0.25);
+  expect(animator(3000, speed)).toEqual(0);
+  expect(animator(3001, speed)).toEqual(2.999);
+  expect(animator(3002, speed)).toEqual(2.998);
+});
+
+
+test("animateScalar with 10 items from 0 to 1 second", () => {
+  const animator = animateScalar([
+    0,
+    0.6858710646629333,
+    2.524005651473999,
+    5.185185432434082,
+    8.340192794799805,
+    11.659809112548828,
+    14.814814567565918,
+    17.47599220275879,
+    19.314130783081055,
+    20
+  ]);
+
+  expect(animator(0)).toEqual(0);
+  expect(animator(1000)).toEqual(0.6858710646629333);
+  expect(animator(2000)).toEqual(2.524005651473999);
+  expect(animator(3000)).toEqual(5.185185432434082);
+  expect(animator(4000)).toEqual(8.340192794799805);
+  expect(animator(5000)).toEqual(11.659809112548828);
+  expect(animator(6000)).toEqual(14.814814567565918);
+  expect(animator(7000)).toEqual(17.47599220275879,);
+  expect(animator(8000)).toEqual(19.314130783081055);
+  expect(animator(9000)).toEqual(20);
+  expect(animator(10000)).toEqual(0.6858710646629333);
+});
+
+function normalizeDecimal(v) {
+  return Number.parseFloat("" + v).toFixed(4);
+}
