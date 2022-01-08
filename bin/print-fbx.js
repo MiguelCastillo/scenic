3704994bb17139f4120caee9e5a6c0ce7f7a52c5
MiguelCastillo <manchagnu@gmail.com>
2022-01-08 17:53:20 -0500

    Adding better printing in fbx file printer
    As Im working thru bigger fbx files that have animation, I have been finding it more tricky to see the nodes in the `Connections` colletion. So I have added printing of the node names in every connection to make it simpler to understand what the connections are actually for.

--- bin/print-fbx.js
+++ bin/print-fbx.js
@@ -6,28 +6,41 @@ import {hideBin} from "yargs/helpers";
 
 import fs from "fs";
 import path from "path";
-import {FbxFile} from "../src/formats/fbxfile.js";
+import {
+  FbxFile,
+  findChildByName,
+} from "../src/formats/fbxfile.js";
 
 let _indentCache = {};
-function _indent(indent) {
-  if (indent in _indentCache === false) {
-    _indentCache[indent] = Array(indent).fill().join("\t");
+function _indent(indent, char="\t") {
+  const k = indent + char;
+  if (k in _indentCache === false) {
+    _indentCache[k] = Array(indent).fill().join(char);
   }
-  return _indentCache[indent];
+  return _indentCache[k];
 }
 
-function printNode(node, indent=1) {
-  console.log(_indent(indent) + node.name + ": " + JSON.stringify(node.attributes) + " {");
+function getName(node) {
+  const nameparts = node ? node.attributes[1].split("\u0000\u0001") : [];
+  return nameparts.length ? [nameparts[1], nameparts[0], node.attributes[2]].filter(Boolean).join("_") : "";
+}
 
-  node.properties.forEach(p => {
-    console.log(_indent(indent+1) + p.name + ": " + JSON.stringify(p.value))
-  });
+function getObjectsByID(model) {
+  const nodeWrappersByID = {
+    "0,0": {
+      connections: [],
+    },
+  };
 
-  node.children.forEach(c => {
-    printNode(c, indent+1);
-  });
+  const objects = findChildByName(model, "Objects");
+  for (const node of objects.children) {
+    nodeWrappersByID[node.attributes[0]] = {
+      node,
+      connections: [],
+    };
+  }
 
-  console.log(_indent(indent) + "}")
+  return nodeWrappersByID;
 }
 
 (function main() {
@@ -42,5 +55,44 @@ function printNode(node, indent=1) {
   const {file} = args;
   const modelBuffer = fs.readFileSync(file);
   const model = FbxFile.fromBinary(modelBuffer.buffer);
+  const objectsByID = getObjectsByID(model);
+
   model.children.forEach(c => printNode(c, 1))
+
+  function printNode(node, indent=1) {
+    console.log(_indent(indent) + node.name + ": " + JSON.stringify(node.attributes) + " {");
+
+    if (node.name === "Connections") {
+      for (const props of node.properties) {
+        switch(props.name) {
+          case "C": {
+            const [_, src, dest] = props.value;
+            const srcName = getName(objectsByID[src].node);
+            const destName = getName(objectsByID[dest].node) || "root";
+            const line = _indent(indent+1) + `${props.name}: ${JSON.stringify(props.value)}`;
+
+            const commentIndent = 60 - line.length;
+
+            console.log(line, _indent(commentIndent, " ") + ` # ${srcName}, ${destName}`);
+            break;
+          }
+          default: {
+            console.log(_indent(indent+1) + `${props.name}: ${JSON.stringify(props.value)}`)
+            break;
+          }
+        }
+      }
+    } else {
+      node.properties.forEach(p => {
+        let sizeInfo = Array.isArray(p.value) ? `*${p.value.length} ` : "";
+        console.log(_indent(indent+1) + `${p.name}: ${sizeInfo}${JSON.stringify(p.value)}`);
+      });
+    }
+
+    node.children.forEach(c => {
+      printNode(c, indent+1);
+    });
+
+    console.log(_indent(indent) + "}")
+  }
 })();
