4dd7ece2a5745e66a6dadcf31312d8cbec8cfde8
MiguelCastillo <manchagnu@gmail.com>
2022-04-02 21:50:02 -0400

    Fixed tests for bump lighting TBN matrices

--- src/math/tbn-matrix.test.js
+++ src/math/tbn-matrix.test.js
@@ -18,15 +18,14 @@ import {
 // loads a model that constains this information.
 
 test("calculateFactor", () => {
-  expect(
-    calculateFactor(
-      ...edgesUV(
-        [0.625,  0.5],
-        [0.875,  0.5],
-        [0.875, 0.75],
-      ),
-    )
-  ).toEqual(16);
+  const [euv1, euv2] = edgesUV(
+    [0.625,  0.5],
+    [0.875,  0.5],
+    [0.875, 0.75],
+  );
+
+  const actual = calculateFactor(euv1, euv2);
+  expect(actual).toEqual(0.0625);
 });
 
 test("calculateTangentVector and calculateBiTangentVector", () => {
@@ -45,10 +44,10 @@ test("calculateTangentVector and calculateBiTangentVector", () => {
   );
 
   const tangent = calculateTangentVector(factor, e1, e2, euv1, euv2);
-  expect(tangent).toEqual([-8, 0, 0]);
+  expect(tangent).toEqual([-0.03125, 0, 0]);
 
   const bitangent = calculateBiTangentVector(factor, e1, e2, euv1, euv2);
-  expect(bitangent).toEqual([0, -8, 0]);
+  expect(bitangent).toEqual([0, -0.03125, 0]);
 
   // tangent and bitangent are orthogonal, so their dotproduct is 0.
   expect(dotproduct(tangent, bitangent)).toEqual(0);
@@ -71,7 +70,11 @@ test("getTBNVectorsFromTriangle and verify vectors are orthogonal", () => {
   expect(dotproduct(tangent, normal)).toEqual(0);
   expect(dotproduct(bitangent, normal)).toEqual(0);
 
-  expect(tangent).toEqual([-1, 0, 0]);
-  expect(bitangent).toEqual([0, -1, 0]);
-  expect(normal).toEqual([0, 0, 1]);
+  expect(tangent.map(_fixZeros)).toEqual([-1, 0, 0]);
+  expect(bitangent.map(_fixZeros)).toEqual([0, 1, 0]);
+  expect(normal.map(_fixZeros)).toEqual([0, 0, 1]);
 });
+
+function _fixZeros(v) {
+  return v === -0 ? 0 : v;
+}
