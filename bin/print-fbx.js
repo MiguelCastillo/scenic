6e42d9a8b3032d5cbfdf562232e99eef19a8c807
MiguelCastillo <manchagnu@gmail.com>
2022-04-24 19:59:46 -0400

    Prettied up files in the `bin` directory
    The very last file to go thru prettier are those in the bin directory.
    
    ```
    $ npx prettier --write .
    ```

--- bin/print-fbx.js
+++ bin/print-fbx.js
@@ -6,14 +6,10 @@ import {hideBin} from "yargs/helpers";
 
 import fs from "fs";
 import path from "path";
-import {
-  FbxFile,
-  findChildByName,
-  getNodeName,
-} from "../src/formats/fbxfile.js";
+import {FbxFile, findChildByName, getNodeName} from "../src/formats/fbxfile.js";
 
 let _indentCache = {};
-function _indent(indent, char="\t") {
+function _indent(indent, char = "\t") {
   const k = indent + char;
   if (k in _indentCache === false) {
     _indentCache[k] = Array(indent).fill().join(char);
@@ -45,50 +41,54 @@ function getObjectsByID(model) {
     .coerce("f", path.resolve)
     .describe("f", "File to load.")
     .demandOption(["f"])
-    .example("print.js -f foo.fbx")
-    .argv;
+    .example("print.js -f foo.fbx").argv;
 
   const {file} = args;
   const modelBuffer = fs.readFileSync(file);
   const model = FbxFile.fromBinary(modelBuffer.buffer);
   const objectsByID = getObjectsByID(model);
 
-  model.children.forEach(c => printNode(c, 1))
+  model.children.forEach((c) => printNode(c, 1));
 
-  function printNode(node, indent=1) {
+  function printNode(node, indent = 1) {
+    // eslint-disable-next-line no-console
     console.log(_indent(indent) + node.name + ": " + JSON.stringify(node.attributes) + " {");
 
     if (node.name === "Connections") {
       for (const props of node.properties) {
-        switch(props.name) {
+        switch (props.name) {
           case "C": {
-            const [_, src, dest] = props.value;
+            const [, src, dest] = props.value;
             const srcName = getNodeName(objectsByID[src].node);
             const destName = getNodeName(objectsByID[dest].node) || "root";
-            const line = _indent(indent+1) + `${props.name}: ${JSON.stringify(props.value)}`;
+            const line = _indent(indent + 1) + `${props.name}: ${JSON.stringify(props.value)}`;
 
             const commentIndent = 60 - line.length;
 
+            // eslint-disable-next-line no-console
             console.log(line, _indent(commentIndent, " ") + ` # ${srcName}, ${destName}`);
             break;
           }
           default: {
-            console.log(_indent(indent+1) + `${props.name}: ${JSON.stringify(props.value)}`)
+            // eslint-disable-next-line no-console
+            console.log(_indent(indent + 1) + `${props.name}: ${JSON.stringify(props.value)}`);
             break;
           }
         }
       }
     } else {
-      node.properties.forEach(p => {
+      node.properties.forEach((p) => {
         let sizeInfo = Array.isArray(p.value) ? `*${p.value.length} ` : "";
-        console.log(_indent(indent+1) + `${p.name}: ${sizeInfo}${JSON.stringify(p.value)}`);
+        // eslint-disable-next-line no-console
+        console.log(_indent(indent + 1) + `${p.name}: ${sizeInfo}${JSON.stringify(p.value)}`);
       });
     }
 
-    node.children.forEach(c => {
-      printNode(c, indent+1);
+    node.children.forEach((c) => {
+      printNode(c, indent + 1);
     });
 
-    console.log(_indent(indent) + "}")
+    // eslint-disable-next-line no-console
+    console.log(_indent(indent) + "}");
   }
 })();
