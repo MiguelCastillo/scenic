d703c83140be8085bd01811a625401c91525ee47
MiguelCastillo <manchagnu@gmail.com>
2022-04-25 21:57:08 -0400

    Addin pm2 for better dev UX
    Im adding pm2 to manage the different build processes and dev server.  I have added few npm script
    
    Start pm2 - this will start all the builds and www server
    ```
    npm run start-dev
    ```
    
    Stop pm2 - this will stop pm2 and clean everything up
    ```
    npm run stop-dev
    ```
    
    Open pm2 console - this will show all the process pm2 has running and their logs
    ```
    npm run monit
    ```
    
    Open pm2 logs - this will open live logs of all the processes pm2 is running.
    ```
    npm run logs
    ```

---
+++ dev.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  apps: [
+    {
+      name: "server",
+      script: "npm",
+      args: "run serve",
+      watch: [".3dub.js"],
+      ignore_watch: ["node_modules"],
+    },
+    {
+      name: "build-scene",
+      script: "npm",
+      args: "run scene -- --watch",
+      watch: ["editor/.pakit-scene.js"],
+      ignore_watch: ["node_modules"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "build-editor",
+      script: "npm",
+      args: "run editor -- --watch",
+      watch: ["editor/.pakit-editor.js"],
+      ignore_watch: ["node_modules"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "build-fileformats",
+      script: "npm",
+      args: "run fileformats -- --watch",
+      watch: ["editor/.pakit-fileformats.js"],
+      ignore_watch: ["node_modules"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
