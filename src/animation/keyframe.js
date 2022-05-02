5a70b9453c3d5cf3a170447eb9c8b53391372fe9
Miguel Castillo <manchagnu@gmail.com>
2022-05-02 18:26:31 -0400

    Cleaning up documenation and code fro keyframe animation (#25)
    Im documenting a bit better the keyframe animation module and cleaned up a couple of redundant and perhaps confusing things in the KeyController.

--- src/animation/keyframe.js
+++ src/animation/keyframe.js
@@ -1,42 +1,32 @@
-// Keyframe animation is based on segments that have a start and an end.
-// The start of a segment is the value of a frame and the end of that
-// same segment is the start of a the next frame. Where a segment ends is
-// exaclty where another one starts; they produce the same value. More on
-// this later.
+// Keyframe animation is the notion that state changes over time in which the
+// state is defined by a frame; frames are snapshots of some state at discrete
+// points in time.
 //
-// In an animation, we iterate thru these segments and want to do that
-// as smoothly as possible over time. In order to do that we interpolate
-// the values of the start and the end of a segement as time goes by. The
-// interpolation between these values are linear over time so we call it
-// linear interpolation. When we interpolate the start and end of a segment
-// we need to measure how much time has gone by since the start of the
-// segment. This measure is called delta and it is a value between 0 and 1
-// where 0 lies exactly on the start of the segment and 1 lies exactly at
-// end of the segment.
+// In some abstract way keyframe animation can also be thought of as a
+// collection of segments with duration t time where the start and end of
+// a segment are two separate frames. In animation, we want to transition
+// between the frames in over the duration of the segment.
 //
-// To illustrate delta, let's assume for a minute that a segment is 1 second
-// long, meaning it takes 1 second to go from the start to the end. At 0
-// seconds we are at the very start of the segment. At 250 milliseconds we
-// have a 0.25 delta which is 1/4 of the way into the segment, at 500
-// milliseconds we have a delta of 0.5 and are at the midpoint of the segment
-// and so on. We can think of this also in terms of percentages where 250
-// milliseconds is 25% of the segment, 500 milliseconds is 50% of the segment
-// and so on. So 20% of a second is 0.2 milliseconds.
+// Imagine an animation with frames `a` and `b` where `a` is the position of a
+// car at t 0 seconds and `b` is the position of the car at t 20 seconds.
+// This has 2 parts:
+// 1. two frames
+// 2. a segment with duration of 20 seconds.
 //
-// More concretely as illustrated below, `s` denotes a segment and we have 3
-// of them. So segment 1 starts at 0 seconds and ends at 1 second. Segment
-// 2 starts at 1 second and ends at 2 seconds. And so on. Now imagine that
-// frame 1 lies on the start of s1 and frame 2 lies on the start of segment
-// 2. When we interpolate with delta we are basically figuring out how much
-// of frame 1 and how much of frame 2 we have to use in order to  generate
-// the final frame. Without interpolation s1 is basically frame 1.
+// The job here is to animation these two frames by interpolating the data from
+// both frames over the course of 20 seconds. Meaning, interpolate the positions
+// of the at frame `a` and `b` so that it seems like the car is smoothly moving
+// between the start and end positions over time.
 //
-// * cp below are curve points, which is a common term used to describe each
-// data point in an animation curve.
-//
-//       s1         s2         s3
-//  |----------|----------|----------|
-// cp0        cp1        cp2        cp3
+// Interpoliation relies on figuring out how much time goes by during the 20
+// seconds of the segment. That number that tells us how far we are until we
+// reach 20 seconds is called delta and we use that value to know how much
+// from frame `a` and how much from `b` we need to use order to know where
+// the car is at any particular time in those 20 seconds. Delta is capped at
+// 1 so that it can be easily used a percentage of time over the duration
+// of the segment. When delta is .25 it means we are taking 25% from frame `a`
+// and 75% from frame `b`. Said differently, the animation is 5 seconds into
+// the segment.
 //
 
 // https://www.khanacademy.org/computing/pixar/animate/ball/pi/animation-with-linear-interpolation
@@ -47,7 +37,12 @@ const lerp = (fraction, min, max, ease = (x) => x) => {
 };
 
 export class KeyController {
-  constructor(frames, times = []) {
+  // Right not we have just two things for animation. Frames and time intervals
+  // for each frame (optional). If we needed more data than those two things,
+  // we can create a KeyFrame object that can hold information about each
+  // frame. Perhaps we want to support different ease function to transition
+  // between frames.
+  constructor(frames, times = [], duration = -1) {
     // Number of frames (segments) in an animation.
     const frameCount = frames.length;
     if (frameCount < 2) {
@@ -57,9 +52,16 @@ export class KeyController {
       );
     }
 
+    if (times.length && frames.length !== times.length) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `KeyController has mismatching number of frames (${frames.length}) and time intervals (${times.length}).`
+      );
+    }
+
     this.frames = frames;
     this.times = times;
-    this.animationLength = times.length ? times[times.length - 1] : -1;
+    this.duration = times.length ? times[times.length - 1] : duration;
     this.segmentCount = frameCount - 1;
   }
 
@@ -73,16 +75,29 @@ export class KeyController {
     // Slow or speed things up! We also take the floor because
     // the decimal points can cause jitters in animations.
     let ms = Math.abs(Math.floor(tms * speed));
-    let idx = Math.floor(ms * 0.001);
-    let len = 1000;
 
-    if (this.times.length) {
-      const times = this.times;
-      const cms = ms % this.animationLength;
+    // idx if the frame index the current time milliseconds (tms) corresponds
+    // to.
+    let idx;
+
+    // len is how long a segment is between two frames. This is because an
+    // animation can be made of frames that last more or less than other
+    // frames.
+    let len;
+
+    // time corresponds to how long each frame is. Sometimes all frames last
+    // the same amount of time, but not always.
+    // If no times exist in the animation then we will default to every frame
+    // lasting 1 second.
+    const times = this.times;
+
+    if (times.length) {
+      const cms = ms % this.duration;
+
       if (!cms && ms) {
-        // We are at the very end of the animation.
-        idx = segmentCount;
-        len = times[idx] - times[idx - 1];
+        // We are at the very end of the animation. So can return early since
+        // there are no more calculations needed here.
+        return [1, segmentCount - 1];
       } else {
         // TODO(miguel): cache index value so that we don't start over the
         // search everytime.
@@ -92,13 +107,12 @@ export class KeyController {
           }
         }
 
-        // Segment length is how long in milliseconds a segment is.
-        // This tells us when we jump to the next keyframe.
         len = times[idx] - times[idx - 1];
         idx--;
       }
-
-      ms = cms;
+    } else {
+      idx = Math.floor(ms * 0.001);
+      len = 1000;
     }
 
     // delta tells us where within a frame range we are so that we can
@@ -110,17 +124,22 @@ export class KeyController {
     // https://techformist.com/problems-with-decimal-multiplication-javascript/
     let delta = (ms % len) / len;
 
-    //
-    //  |----------|----------|
-    // cp0        cp1        cp2
-    //
     if (!delta && idx) {
-      // This adjustment allows us to access all the data in the very
-      // last frame. Basically instead of taking frame X with delta
-      // 0, we take frame - X with delta of 1. It will yield the same
-      // result, but we do this adjustment so that we can access the very
-      // last datapoint in a frame while staying withing range in the
-      // array of frame data.
+      // The adjustment in this logic branch allows us to access all the data
+      // in the very last frame while generating array indexes that can
+      // interpolate frames with
+      // lerp(delta, this.frames[index], this.frames[index + 1]).
+      //
+      // Without this adjustment,  we would generate frame indexes that will
+      // go out of bound when we hit `this.frames[index + 1]`.
+      //
+      // So we return the index for cp1 and the delta of 1 which tells lerp to
+      // use the value at cp2. Which is exactly lerp's job.
+      //
+      //  |----------|----------|
+      // cp0        cp1        cp2
+      //
+
       delta = 1;
       idx--;
     }
@@ -128,7 +147,7 @@ export class KeyController {
     idx = idx % segmentCount;
 
     if (speed < 0) {
-      // If we are in reverse mode then we just invert the current state.
+      // If we are in reverse mode then we just invert the computed values.
       idx = segmentCount - idx - 1;
       delta = 1 - delta;
     }
